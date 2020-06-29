8a8e6c911bcde6556ff0e672bf3fcd5b47c9029e
彭萨萨 <pengsasa@qingzu.com.cn>
2020-06-29 16:04:34 +0800

    45145

---
+++ src/main/java/com/test/concurrentdemo/ReentrantLockTest/demo3/ReentTrantLockTest.java
@@ -0,0 +1,30 @@
+package com.test.concurrentdemo.ReentrantLockTest.demo3;
+
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 测试公平锁与非公平锁
+ */
+public class ReentTrantLockTest {
+
+    static ReentrantLock lock = new ReentrantLock(true);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> test()).start();
+        }
+    }
+
+    public static void test() {
+        try {
+            System.out.println(Thread.currentThread().getName() + "开始获取锁");
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + "获取到了锁==============");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
