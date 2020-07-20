e36801a6ccc046113444de1a98318572e7d250ed
彭萨萨 <pengsasa@qingzu.com.cn>
2020-07-20 16:15:30 +0800

    信号量

---
+++ src/main/java/com/test/concurrentdemo/SemaphoreDemo/SemaphoreTest.java
@@ -0,0 +1,33 @@
+package com.test.concurrentdemo.SemaphoreDemo;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * 信号量 是用来控制同时访问特定资源的线程数量，它通过协调各个线程，以保证合理的使用公共资源
+ * 可以理解为加强的Synchronized
+ * 也可以用来限流!
+ */
+public class SemaphoreTest {
+    //初始化3个许可证的 信号量
+    static Semaphore semaphore = new Semaphore(3);
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 4; i++) {
+            int finalI = i;
+            new Thread(()->{
+                try {
+                    //获取一个许可证
+                    semaphore.acquire();
+                    System.out.println("我是" + finalI + "号.");
+                    //剩余的许可证数量
+                    System.out.println(semaphore.availablePermits());
+                    Thread.sleep(2000);
+                    //释放一个许可证
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
