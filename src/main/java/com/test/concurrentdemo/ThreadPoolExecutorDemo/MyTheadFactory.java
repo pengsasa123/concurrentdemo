88a2819d9e0186ed2b099f62f1d356612fef6a63
彭萨萨 <pengsasa@qingzu.com.cn>
2020-09-11 16:27:36 +0800

    线程工厂自定义名字

---
+++ src/main/java/com/test/concurrentdemo/ThreadPoolExecutorDemo/MyTheadFactory.java
@@ -0,0 +1,40 @@
+package com.test.concurrentdemo.ThreadPoolExecutorDemo;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 自定义线程工厂
+ */
+public class MyTheadFactory implements ThreadFactory {
+
+    private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+
+    MyTheadFactory() {
+        this("pool-");
+    }
+
+    MyTheadFactory(String name) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() :
+            Thread.currentThread().getThreadGroup();
+        namePrefix = name +
+            poolNumber.getAndIncrement() +
+            "-thread-";
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r,
+            namePrefix + threadNumber.getAndIncrement(),
+            0);
+        if (t.isDaemon())
+            t.setDaemon(false);
+        if (t.getPriority() != Thread.NORM_PRIORITY)
+            t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+}
---
+++ src/main/java/com/test/concurrentdemo/ThreadPoolExecutorDemo/ThreadPoolExecutorTest2.java
@@ -0,0 +1,41 @@
+package com.test.concurrentdemo.ThreadPoolExecutorDemo;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 自定义一个线程池
+ */
+public class ThreadPoolExecutorTest2 {
+    /**
+     * 获取处理器数目
+     */
+    private static int availableProcessors = Runtime.getRuntime().availableProcessors();
+
+    private static AtomicInteger count = new AtomicInteger(1);
+    /**
+     * 基于LinkedBlockingQueue
+     */
+    private static BlockingQueue queue = new LinkedBlockingQueue(20);
+
+    static ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(
+        availableProcessors * 2,
+        availableProcessors * 2,
+        0,
+        TimeUnit.SECONDS,
+        //阻塞队列
+        queue,
+        //线程工厂
+        new MyTheadFactory("mypool-")
+    );
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            poolExecutor.execute(() -> System.out.println(Thread.currentThread().getName()));
+        }
+    }
+
+}
